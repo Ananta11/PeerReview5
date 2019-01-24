9bc9c6c020b77191cea1be554820516c2c67e203
Ananta Shahane <ananta@Anantas-MacBook-Air.local>
2019-01-24 15:53:50 +0530

    Readme Added.

--- PeerReview5/ViewController.m
+++ PeerReview5/ViewController.m
@@ -7,9 +7,22 @@
 //
 
 #import "ViewController.h"
+#import "Social/Social.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *FacebookTextView;
+@property (weak, nonatomic) IBOutlet UITextView *TweetTextView;
+@property (weak, nonatomic) IBOutlet UITextView *MoreTextView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *FacebookPostButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *TweetButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *ShareButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *NullButton;
 
+- (void) showAlertMessage:(NSString *) myMessage;
+
+- (void) configureFacebookTextView;
+- (void) configureTwitterTextView;
+- (void) configureMoreTextView;
 @end
 
 @implementation ViewController
@@ -17,6 +30,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self configureFacebookTextView];
+    [self configureTwitterTextView];
+    [self configureMoreTextView];
 }
 
 
@@ -25,5 +41,129 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) showAlertMessage:(NSString *) myMessage{
+    UIAlertController *alertController;
+    alertController = [UIAlertController alertControllerWithTitle:@"PeerReview5" message:myMessage preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil]];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+- (IBAction)ShareNothing:(id)sender {
+    if([self.FacebookTextView isFirstResponder])
+    {
+        [self.FacebookTextView resignFirstResponder];
+    }
+    if([self.TweetTextView isFirstResponder])
+    {
+        [self.TweetTextView resignFirstResponder];
+    }
+    if([self.MoreTextView isFirstResponder])
+    {
+        [self.MoreTextView resignFirstResponder];
+    }
+    NSString *message = @"This button does nothing.";
+    [self showAlertMessage:message];
+}
+
+- (IBAction)shareMore:(id)sender {
+    self.FacebookTextView.layer.borderColor = self.FacebookTextView.layer.backgroundColor;
+    if([self.FacebookTextView isFirstResponder])
+    {
+        [self.FacebookTextView resignFirstResponder];
+    }
+    if([self.TweetTextView isFirstResponder])
+    {
+        [self.TweetTextView resignFirstResponder];
+    }
+    if([self.MoreTextView isFirstResponder])
+    {
+        [self.MoreTextView resignFirstResponder];
+    }
+    
+    UIActivityViewController *moreVC = [[UIActivityViewController alloc] initWithActivityItems:@[self.MoreTextView.text] applicationActivities:nil];
+    
+    [self presentViewController:moreVC animated:YES completion:nil];
+}
+
+
+- (IBAction)shareTweet:(id)sender {
+    if([self.FacebookTextView isFirstResponder])
+    {
+        [self.FacebookTextView resignFirstResponder];
+    }
+    if([self.TweetTextView isFirstResponder])
+    {
+        [self.TweetTextView resignFirstResponder];
+    }
+    if([self.MoreTextView isFirstResponder])
+    {
+        [self.MoreTextView resignFirstResponder];
+    }
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
+    {
+        SLComposeViewController *twitterVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        if([self.TweetTextView.text length] <= 140)
+        {
+            [twitterVC setInitialText:self.TweetTextView.text];
+        }
+        else
+        {
+            [twitterVC setInitialText:[self.TweetTextView.text substringFromIndex:140]];
+        }
+        [self presentViewController:twitterVC animated:YES completion:nil];
+    }
+    else
+    {
+        [self showAlertMessage:@"Please sign your device in to twitter \nHint\nGo to: Settings>Twitter."];
+    }
+}
+
+- (IBAction)shareFacebook:(id)sender {
+    if([self.FacebookTextView isFirstResponder])
+    {
+        [self.FacebookTextView resignFirstResponder];
+    }
+    if([self.TweetTextView isFirstResponder])
+    {
+        [self.TweetTextView resignFirstResponder];
+    }
+    if([self.MoreTextView isFirstResponder])
+    {
+        [self.MoreTextView resignFirstResponder];
+    }
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook])
+    {
+        SLComposeViewController *facebookVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        [facebookVC setInitialText:self.FacebookTextView.text];
+        [self presentViewController:facebookVC animated:YES completion:nil];
+    }
+    else
+    {
+        [self showAlertMessage:@"Please sign your device in to Facebook \nHint\nGo to: Settings>Facebook."];
+    }
+}
+
+- (void) configureFacebookTextView{
+    self.FacebookTextView.layer.backgroundColor = [UIColor colorWithRed:0.23 green:0.34 blue:0.59 alpha:1].CGColor;
+    self.FacebookTextView.layer.cornerRadius=15.0;
+    self.FacebookTextView.layer.borderWidth = 2.0;
+    self.FacebookTextView.layer.borderColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0].CGColor;
+}
+
+- (void) configureTwitterTextView{
+    self.TweetTextView.layer.backgroundColor = [UIColor colorWithRed:0.22 green:0.63 blue:0.94 alpha:1].CGColor;
+    self.TweetTextView.layer.cornerRadius = 15.0;
+    self.TweetTextView.layer.borderColor= [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0].CGColor;
+    self.TweetTextView.layer.borderWidth = 2.0;
+}
+
+-(void) configureMoreTextView{
+    self.MoreTextView.layer.backgroundColor = [UIColor colorWithRed:0.35 green:0.25 blue:0.50 alpha:1].CGColor;
+    self.MoreTextView.layer.cornerRadius = 15.0;
+    self.MoreTextView.layer.borderColor= [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0].CGColor;
+    self.MoreTextView.layer.borderWidth = 2.0;
+}
 
 @end
---
+++ README.md
@@ -0,0 +1,14 @@
+# Peer Review 5
+#### A simple project for creating a small all in one share portal.
+##### This project is 5/15 Peer Review projects done in the course \"iOS Development for Creative Entrepreneurs\" by UCI.
+
+----
+
+This is a small share portal which uses all the default share methods available in iOS 8. The top most *UITextView* is a used to share the text in that view using Facebook, the second is for Tweeting and the third one has is used to share text using sharesheet.
+
+The *UIToolBar* at the top has 4 share *UIButton*s, starting from left,
+the first one shares the text in top most *UITextView* to Facebook, second one is for *Tweeting* from second view the third button will pull up a share sheet for text in the last view. The fourth one does nothing but present a *UIAllertView*.
+
+___
+
+![PeerReview5](https://github.com/Ananta11/PeerReview5/raw/master/Common/Screenshot.png)
